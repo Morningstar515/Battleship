8f64e1a25b5639ec68e903b80cf856c7e6ae1f39
Morningstar515 <tmains515@hotmail.com>
2023-12-23 13:05:31 -0800

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+    content: ["./src/index.html",
+              "./src/index.js"],
+    theme: {
+    extend: {
+        backgroundImage: {
+        }
+    },
+    },
+    plugins: [],
+}
